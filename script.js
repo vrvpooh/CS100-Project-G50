274356006c4288caeba5baa87fd2f028574dd957
Nonnarit Rammaneekultawat <nonnarit.ram@dome.tu.ac.th>
2023-12-09 19:01:44 +0700

    change bg fix bug

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-})
+});
 
 // Function to validate Firstname and Lastname
 function validateName() {
